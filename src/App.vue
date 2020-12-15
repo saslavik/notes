9df29cd562371419b6bd7d0857a3dca9144d6f76
saslavik <saslavik@gmail.com>
2020-12-15 22:16:21 +0200

    remaining 2 mod

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,18 @@
       <section>
         <div class="container">
           <div id="app">
-            <h3>{{ title }}</h3>
             <message :message='message' v-if="message"/>
             <!-- new note -->
-            <div class="new-note">
-              <input type="text" v-model="note.title">
-              <textarea v-model="note.descr"></textarea>
-              <button @click.prevent="addNote()">New note</button>
-            </div>
-            <!-- note list -->
-            <div class="notes">
-              <div class="note" v-for="(note, index) in notes" :key="index">
-                <div class="note-header">
-                  <p>{{ note.title }}</p>
-                </div>
-                <div class="note-body">
-                  <p>{{ note.descr }}</p>
-                  <span>{{ note.date }}</span>
-                </div>
+            <newNote :note="note" @addNote="addNote()" />
+            <div class="note-header">
+              <h3>{{ title }}</h3>
+              <div class="icons">
+                <svg :class="{ active: grid }" @click="grid = true" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+                <svg :class="{ active: !grid }" @click="grid = false" style="cursor: pointer;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
               </div>
             </div>
+            <!-- note list -->
+            <notes :notes="notes" :grid="grid" @remove="removeNote"/>
           </div>
         </div>
       </section>
@@ -34,12 +26,15 @@
 
 <script>
 import message from '@/components/Message.vue';
+import newNote from '@/components/NewNote.vue';
+import notes from '@/components/Notes.vue';
 
 export default {
   data() {
     return {
       title: 'Notes app',
       message: null,
+      grid: true,
       note: {
         title: '',
         descr: '',
@@ -64,7 +59,9 @@ export default {
     };
   },
   components: {
-    message
+    message,
+    notes,
+    newNote,
   },
   methods: {
     addNote() {
@@ -82,6 +79,9 @@ export default {
       this.note.descr = '';
       this.message = null;
     },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+    }
   },
 }
 </script>
---
+++ src/components/NewNote.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- new note -->
+  <div class="new-note">
+    <label>Title</label>
+    <input type="text" v-model="note.title">
+    <label>Description</label>
+    <textarea v-model="note.descr"></textarea>
+    <button class="btn btnPrimary" @click.prevent="addNote()">New note</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    note: {
+      type: Object,
+      required: true,
+    }
+  },
+  methods: {
+    addNote() {
+      this.$emit('addNote', this.note)
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.new-note {
+  text-align: center;
+}
+</style>
